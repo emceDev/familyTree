bf421e2e30ad307679b6f7900a5ebc4f6b4c550d
m4t1ce <m4t1ce@gmail.com>
2021-01-19 20:44:31 +0100

    vars work

--- src/db/Config.js
+++ src/db/Config.js
@@ -9,5 +9,4 @@ export const firebaseConfig = {
 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_APP_ID,
 };
-console.log(firebaseConfig);
 export const app = firebase.initializeApp(firebaseConfig);
